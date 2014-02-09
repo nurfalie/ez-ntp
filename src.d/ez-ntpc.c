81b43f85fccb7d23d76e506695b1815d0e282c23
textfield <textfield@d8d70885-5c4c-0410-975c-8a065988bd78>
2014-02-09 15:49:33 +0000

    Minor updates.

--- src.d/ez-ntpc.c
+++ src.d/ez-ntpc.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2005 - 2014 Alexis Megas.
+** Copyright (c) 2005 - Eternity Alexis Megas.
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
