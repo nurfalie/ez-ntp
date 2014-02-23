b9ca11ef2ec1087ef202d24c1d36e667111840be
textfield <textfield@d8d70885-5c4c-0410-975c-8a065988bd78>
2014-02-23 01:07:17 +0000

    Format changes.

--- src.d/ez-ntpc.sh
+++ src.d/ez-ntpc.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 case "$1" in
-start)
+    start)
         if [ -e /var/run/ez-ntpc.pid ]
 	then
-    		echo "The EzNTP client daemon may already be running."
-    		exit 1
+    	    echo "The EzNTP client daemon may already be running."
+    	    exit 1
 	fi
 
 	echo "Starting the EzNTP client daemon."
 	/usr/local/bin/ez-ntpc -p 50000 -h 192.168.178.1
 	;;
-stop)
+    stop)
         if [ ! -e /var/run/ez-ntpc.pid ]
 	then
-	        echo "The EzNTP client daemon is not running."
-		exit 1
+	    echo "The EzNTP client daemon is not running."
+	    exit 1
 	else
-	        echo "Terminating the EzNTP client daemon."
-	        kill -TERM `cat /var/run/ez-ntpc.pid`
+	    echo "Terminating the EzNTP client daemon."
+	    kill -TERM `cat /var/run/ez-ntpc.pid`
 	fi
 
         ;;
-*)
+    *)
 	echo "Usage: /etc/init.d/ez-ntpc.sh {start|stop}"
 	exit 1
 	;;
--- src.d/ez-ntpd.sh
+++ src.d/ez-ntpd.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 case "$1" in
-start)
+    start)
         if [ -e /var/run/ez-ntpd.pid ]
 	then
-    		echo "The EzNTP daemon may already be running."
-    		exit 1
+    	    echo "The EzNTP daemon may already be running."
+    	    exit 1
 	fi
 
 	echo "Starting the EzNTP daemon."
 	/usr/local/bin/ez-ntpd
 	;;
-stop)
+    stop)
         if [ ! -e /var/run/ez-ntpd.pid ]
 	then
 	        echo "The EzNTP daemon is not running."
 		exit 1
 	else
-	        echo "Terminating the EzNTP daemon."
-	        kill -TERM `cat /var/run/ez-ntpd.pid`
+	    echo "Terminating the EzNTP daemon."
+	    kill -TERM `cat /var/run/ez-ntpd.pid`
 	fi
 
         ;;
-*)
+    *)
 	echo "Usage: /etc/init.d/ez-ntpd.sh {start|stop}"
 	exit 1
 	;;
